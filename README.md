6fd285ba12a79a032331705622a970ec02c6c0fe
PTFOPlayer <PTFOPlayer>
2023-01-20 14:42:53 +0100

    New Readme and Simple euclides

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # euclides
-My implementation of extended euclides algorithm
+Simple implementation of euclides algorithm
+
+## Example usage:
+### Extended
+> Program
+```
+use euc_lib;
+fn main() {
+    prinln!("{:?}, euc_lib::euc_ext(135, 35)");
+}
+```
+> Output
+```
+EucRes { d: 5, s: -1, t: 4 }
+```
+### Simple
+> Program
+```
+use euc_lib;
+fn main() {
+    prinln!("{}, euc_lib::euc(135, 35)");
+}
+```
+> Output
+```
+5
+```
--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,18 @@ pub fn euc_ext(d1:i32, d2:i32) -> EucRes {
         t: t[i]
     };
 }
+
+pub fn euc(d1:i32, d2:i32) -> i32 {
+    let mut d = vec![d1, d2];
+    let mut q = vec![0, (d1 - d1%d2) / d2];
+
+    let mut i = 2;
+    while d[i-1] != 0 {
+        d.append(&mut vec![ d[i-2] % d[i-1] ]);
+        if d[i] == 0 {break;}
+        q.append(&mut vec![ (d[i-1] - (d[i-1]%d[i])) / d[i] ]);
+        i += 1;
+    }
+    i-=1;
+    d[i]
+}
