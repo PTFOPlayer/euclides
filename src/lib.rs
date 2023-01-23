f3bd509d01124751f3b614be199b93b5a6e995e0
PTFOPlayer <PTFOPlayer>
2023-01-23 11:48:50 +0100

    0.1.7-rc2 improved performance

--- src/lib.rs
+++ src/lib.rs
@@ -5,37 +5,36 @@ pub struct EucRes {
     pub t: i32,
 }
 
-pub fn euc_ext(d1: i32, d2: i32) -> EucRes {
-    let mut d = vec![d1, d2];
-    let mut s = vec![1, 0];
-    let mut t = vec![0, 1];
-    let mut qp = d1 / d2;
+pub fn euc_ext(mut d1: i32, mut d2: i32) -> EucRes {
+    let mut spp = 1;
+    let mut sp = 0;
+    let mut tpp = 0;
+    let mut tp = 1;
+    let mut q = d1 / d2;
 
-    let mut i = 2;
-    while d2 != 0 {
-        d.append(&mut vec![d[i - 2] % d[i - 1]]);
-        if d[i] == 0 {
+    loop {
+        (d1, d2) = (d2, d1 % d2);
+
+        if d2 == 0 {
             break;
         }
-        s.append(&mut vec![s[i - 2] - (qp * s[i - 1])]);
-        t.append(&mut vec![t[i - 2] - (qp * t[i - 1])]);
-        let q = d[i - 1] / d[i];
-        qp = q;
-        i += 1;
+
+        (spp, sp) = (sp, spp - (q * sp));
+        (tpp, tp) = (tp, tpp - (q * tp));
+
+        q = d1 / d2;
     }
 
     return EucRes {
-        d: d[i - 1],
-        s: s[i - 1],
-        t: t[i - 1],
+        d: d1,
+        s: sp,
+        t: tp,
     };
 }
 
 pub fn euc(mut d1: i32, mut d2: i32) -> i32 {
     while d2 != 0 {
-        let t = d2;
-        d2 = d1 % d2;
-        d1 = t.clone();
+        (d1, d2) = (d2, d1 % d2);
     }
     return d1;
 }
@@ -48,10 +47,10 @@ pub fn euc_recursive(d1: i32, d2: i32) -> i32 {
     }
 }
 
-pub fn lcm(d1:i32, d2:i32) -> i32 {
+pub fn lcm(d1: i32, d2: i32) -> i32 {
     let mut temp = d1 * d2;
     if temp < 0 {
         temp = temp * -1;
     }
-    return temp/euc(d1, d2);
+    return temp / euc(d1, d2);
 }
